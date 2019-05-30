44c258b274988ca7f042f5b1837c860fe1191c25
Cris <craisochoa22@gmail.com>
2019-05-29 21:54:10 -0600

    Cambios

--- src/main/resources/static/js/general-calendar.js
+++ src/main/resources/static/js/general-calendar.js
@@ -2,14 +2,29 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('general-calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: ['dayGrid', 'timeGrid'],
+        plugins: ['interaction','dayGrid', 'timeGrid'],
         events: '/eventos',
         header: {
             left:   'dayGridMonth,timeGridWeek,timeGridDay',
-            center: 'title',
+            center: 'addEventButton, title',
             right:  'today prev,next'
+        },
+        customButtons: {
+            addEventButton: {
+                text: 'add event...',
+                click: function () {
+
+                }
+            }
+        },
+        navLinks: true,
+        selectable: true,
+        select: function (info) {
+
         }
     });
 
     calendar.render();
+
+
 });
